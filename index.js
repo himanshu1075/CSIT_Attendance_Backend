2f0739eab6cef86b00228d6f6fc9234b002aaf7f
Jaydeep Singh Chouhan <jaydeepchouhan20808@acropolis.in>
2023-04-21 00:36:36 +0530

    fixing port issue

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import configureDB from './src/configs/dbConfig.js';
 import studentRouter from './src/routers/studentRouter.js';
 import cors from "cors";
 import facultyRouter from './src/routers/facultyRouter.js';
-
+const port = process.env.PORT || 5500;
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -13,7 +13,8 @@ app.use(express.json());
 app.use(studentRouter);
 app.use(facultyRouter);
 
-app.listen((process.env.PORT || 5500), ()=> {
+app.listen(port, ()=> {
     configureDB();
-    console.log(`Server is running on Port ${ process.env.SERVER_PORT}`);
+    console.log(`Server is running on Port ${port}`);
+
 })
